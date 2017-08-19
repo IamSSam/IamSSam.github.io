6f020302c0ba19d09b3fb9fd9e1cb4cdf0e9b822
IamSSam <kongse92@gmail.com>
2017-08-19 16:11:03 +0900

    Modify files

--- webview/webview.js
+++ webview/webview.js
@@ -1,4 +1,5 @@
 var map;
+var mytest = [];
 
 var infowindow = new naver.maps.InfoWindow();
 
@@ -19,17 +20,31 @@ window.onload = function(){
 }
 
 function onSuccessGeolocation(position) {
-    var location = new naver.maps.LatLng(position.coords.latitude,
-                                         position.coords.longitude);
+  var location = new naver.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+  console.log(position.coords.latitude+"   "+position.coords.longitude);
+  naver.maps.Service.reverseGeocode({
+      location: new naver.maps.LatLng(position.coords.latitude, position.coords.longitude),
+  }, function(status, response) {
+      if (status !== naver.maps.Service.Status.OK) {
+          return alert('Something wrong!');
+      }
+      var result = response.result, // 검색 결과의 컨테이너
+          items = result.items; // 검색 결과의 배열
+        for(var i=0; i<items.length; i++)
+        {
+          mytest.push(items[i].address);
+        }
 
-    map.setCenter(location); // 얻은 좌표를 지도의 중심으로 설정합니다.
-    map.setZoom(10); // 지도의 줌 레벨을 변경합니다.
+        map.setCenter(location); // 얻은 좌표를 지도의 중심으로 설정합니다.
+        map.setZoom(10); // 지도의 줌 레벨을 변경합니다.
 
-    infowindow.setContent('<div style="padding:20px;">' +
-        'latitude: '+ location.lat() +'<br />' +
-        'longitude: '+ location.lng() +'</div>');
+        infowindow.setContent('<div style="padding:20px;">' +
+           mytest[1]+'</div>');
 
-    infowindow.open(map, location);
+        infowindow.open(map, location);
+      // do Something
+  });
 }
 
 function onErrorGeolocation() {
@@ -42,59 +57,75 @@ function onErrorGeolocation() {
 }
 
 
-// function getMapElement() {
-//   var HOME_PATH = window.HOME_PATH || '.';
-//     var myaddress = document.getElementById("myinput").value;// 도로명 주소나 지번 주소만 가능 (건물명 불가!!!!)
-//     var map = new naver.maps.Map('map',{
-//         scaleControl: false,
-//           logoControl: false,
-//           mapDataControl: false,
-//           zoomControl: true,
-//           minZoom: 1
-//     });
-//       naver.maps.Service.geocode({address: myaddress}, function(status, response) {
-//           if (status !== naver.maps.Service.Status.OK) {
-//               return alert(myaddress + '의 검색 결과가 없거나 기타 네트워크 에러');
-//           }
-//           var result = response.result;
-//           // 검색 결과 갯수: result.total
-//           // 첫번째 결과 결과 주소: result.items[0].address
-//           // 첫번째 검색 결과 좌표: result.items[0].point.y, result.items[0].point.x
-//           var myaddr = new naver.maps.Point(result.items[0].point.x, result.items[0].point.y);
-//           var latlng = new naver.maps.LatLng((result.items[0].point.x),(result.items[0].point.y)); // 위도,경도 좌표계
-//           var utmk = naver.maps.TransCoord.fromLatLngToUTMK(latlng); // 위/경도 -> UTMK
-//           var tm128 = naver.maps.TransCoord.fromUTMKToTM128(utmk);   // UTMK -> TM128
-//           var naverCoord = naver.maps.TransCoord.fromLatLngToNaver(latlng); // TM128 -> NAVER
-//
-//           map.setCenter(myaddr); // 검색된 좌표로 지도 이동
-//           // 마커 표시
-//           var marker = new naver.maps.Marker({
-//             position: myaddr,
-//             map: map
-//           });
-//           var contentString = [
-//             '<div style="padding:10px;width:300px;font-size:14px;line-height:20px;">',
-//             '<strong>LatLng</strong> : '+ latlng +'<br />',
-//             '<strong>UTMK</strong> : '+ utmk +'<br />',
-//             '<strong>TM128</strong> : '+ tm128 +'<br />',
-//             '<strong>NAVER</strong> : '+ naverCoord +'<br />',
-//             '</div>'
-//             ].join('');
-//           // 마커 클릭 이벤트 처리
-//           naver.maps.Event.addListener(marker, "click", function(e) {
-//             if (infowindow.getMap()) {
-//                 infowindow.close();
-//             } else {
-//                 infowindow.open(map, marker);
-//
-//             }
-//           });
-//
-//           // 마크 클릭시 인포윈도우 오픈
-//           var infowindow = new naver.maps.InfoWindow({
-//               content: contentString
-//           });
-//       });
-//   // var mapDiv = document.getElementById('map'); // 'map' 으로 선언해도 동일
-//   // var map = new naver.maps.Map(mapDiv);
-// }
+function getMapElement() {
+  var HOME_PATH = window.HOME_PATH || '.';
+    var myaddress = document.getElementById("myinput").value;// 도로명 주소나 지번 주소만 가능 (건물명 불가!!!!)
+    var map = new naver.maps.Map('map',{
+        scaleControl: false,
+          logoControl: false,
+          mapDataControl: false,
+          zoomControl: true,
+          minZoom: 1
+    });
+      naver.maps.Service.geocode({address: myaddress}, function(status, response) {
+          if (status !== naver.maps.Service.Status.OK) {
+              return alert(myaddress + '의 검색 결과가 없거나 기타 네트워크 에러');
+          }
+          var result = response.result;
+          // 검색 결과 갯수: result.total
+          // 첫번째 결과 결과 주소: result.items[0].address
+          // 첫번째 검색 결과 좌표: result.items[0].point.y, result.items[0].point.x
+          var myaddr = new naver.maps.Point(result.items[0].point.x, result.items[0].point.y);
+          var latlng = new naver.maps.LatLng((result.items[0].point.x),(result.items[0].point.y)); // 위도,경도 좌표계
+          var utmk = naver.maps.TransCoord.fromLatLngToUTMK(latlng); // 위/경도 -> UTMK
+          var tm128 = naver.maps.TransCoord.fromUTMKToTM128(utmk);   // UTMK -> TM128
+          var naverCoord = naver.maps.TransCoord.fromLatLngToNaver(latlng); // TM128 -> NAVER
+
+          map.setCenter(myaddr); // 검색된 좌표로 지도 이동
+          // 마커 표시
+          var marker = new naver.maps.Marker({
+            position: myaddr,
+            map: map
+          });
+          var contentString = [
+            '<div style="padding:10px;width:300px;font-size:14px;line-height:20px;">',
+            '<strong>LatLng</strong> : '+ latlng +'<br />',
+            '<strong>UTMK</strong> : '+ utmk +'<br />',
+            '<strong>TM128</strong> : '+ tm128 +'<br />',
+            '<strong>NAVER</strong> : '+ naverCoord +'<br />',
+            '</div>'
+            ].join('');
+          // 마커 클릭 이벤트 처리
+          naver.maps.Event.addListener(marker, "click", function(e) {
+            if (infowindow.getMap()) {
+                infowindow.close();
+            } else {
+                infowindow.open(map, marker);
+
+            }
+          });
+
+          // 마크 클릭시 인포윈도우 오픈
+          var infowindow = new naver.maps.InfoWindow({
+              content: contentString
+          });
+      });
+  // var mapDiv = document.getElementById('map'); // 'map' 으로 선언해도 동일
+  // var map = new naver.maps.Map(mapDiv);
+}
+
+function getAh(){
+  var post_data = {post_data:mytest[1]};
+  $.ajax({
+    url: '../php/location.php',
+    type: 'POST',
+    data: post_data,
+    dataType: 'json',
+    success: function(data){
+      console.log("asdasd: " + data);
+    },
+    error: function(request, status, error){
+      console.log(request, status, error);
+    },
+  });
+}
