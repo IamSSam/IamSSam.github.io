f0d206a9ec5bcd3d72940e6b334cc0b53db16691
IamSSam <kongse92@gmail.com>
2017-08-20 00:49:53 +0900

    Modify files

--- webview/php/location.php
+++ webview/php/location.php
@@ -1,4 +1,5 @@
 <?php
+  Route::get('/ajax/populateApiAuth', 'ApiController@populateApiAuth');
   header("Content-Type:application/json; charset=utf-8");
   header('Access-Control-Allow-Origin: *');
   header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
